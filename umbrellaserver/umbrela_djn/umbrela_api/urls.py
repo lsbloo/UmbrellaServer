a34974cf2e866af5379b2cf418c19bb644e595b2
lsbloo <osvaldo_cavalcanti_filho@hotmail.com>
2019-06-04 23:50:00 -0300

    kk

--- umbrellaserver/umbrela_djn/umbrela_api/urls.py
+++ umbrellaserver/umbrela_djn/umbrela_api/urls.py
@@ -17,8 +17,29 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include,url
+from umbrella_application.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^', include('umbrella-app.urls')),
+    url(r'umbrella/api/v1/mgmnt/manager/g/$', views.GestorList.as_view(),name='gestores_list'),
+    url(r'umbrella/api/v1/mgmnt/users/g/$', views.UsersList.as_view(),name="users_list"),
+    url(r'umbrella/api/v1/mgmnt/followers/g/$', views.SeguidoresList.as_view(),name="followers_list"),
+    url(r'umbrella/api/v1/mgmnt/profiles/g/$',views.PerfisList.as_view(),name="perfis_list"),
+    url(r'umbrella/api/v1/mgmnt/feedback/g/$',views.FeedbacksList.as_view(),name="feedback_list"),
+    url(r'umbrella/api/v1/mgmnt/posts/g/$',views.PostList.as_view(),name="posts_list"),
+    url(r'umbrella/api/v1/mgmnt/followers/cg/', views.SeguidoresViewSet.as_view(
+        {'get': 'list',
+        'post': 'create'}
+    ) , name="create_followers"),
+    url(r'umbrella/api/v1/mgmnt/profiles/c/$', views.CreateProfiles.as_view(
+        {'post': 'create'}
+
+    ) , name="profile_create"),
+
+    url(r'umbrella/api/v1/mgmnt/users/c/$', views.CreateUser.as_view(
+        {'post': 'create'}
+    ), name="users_create"),
+    url(r'umbrella/api/v1/mgmnt/tags/g' , views.TagsList.as_view(),name='tags_list'),
+
+    url(r'api-token-auth/' , views.CustomAuthToken.as_view(), name="user_token_list"),
 ]
