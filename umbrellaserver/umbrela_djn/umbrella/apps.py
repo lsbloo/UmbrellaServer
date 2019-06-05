45625e3cb5e0c9551524bd3301bb89fe549939a0
lsbloo <osvaldo_cavalcanti_filho@hotmail.com>
2019-06-04 23:38:11 -0300

    delete_mode

--- umbrellaserver/umbrela_djn/umbrella/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UmbrellaConfig(AppConfig):
-    name = 'umbrella'
--- umbrellaserver/umbrela_djn/umbrella/generators/token_user.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.authtoken.models import Token
-"""
-Cria um token aleatorio para uma instancia user especificada;
-        -> Return true se a criação for sucesso;
-"""
-def gerenate_token(my_instance_of_user):
-        Token.objects.create(user=my_instance_of_user)
-        return True
