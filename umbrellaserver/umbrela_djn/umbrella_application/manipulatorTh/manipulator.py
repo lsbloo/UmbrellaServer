06e10b8f734a8997b6d5d7a3dbc213d0f343bca5
OsvaldoAiron <osvaldo_cavalcanti_filho@hotmail.com>
2019-06-20 22:00:52 -0300

    validando_identificador_execução_thread

--- umbrellaserver/umbrela_djn/umbrella_application/manipulatorTh/manipulator.py
+++ umbrellaserver/umbrela_djn/umbrella_application/manipulatorTh/manipulator.py
@@ -16,8 +16,23 @@ class Manipulator(object):
         self.identifier.append(identifier)
     
     def add_thread(self,UmbrellaBot,identifier):
-        for i in self.identifier:    
-            if i == identifier:   
-                self.dict_th[identifier] = UmbrellaBot
-                print("Thread Add")
+        result = self.checkExistenceIdentifier(identifier)
+        if result == True:
+            print("Identifier Exists in manipulator.")
+        else:
+            for i in self.identifier:    
+                if i == identifier:   
+                    self.dict_th[identifier] = UmbrellaBot
+                    print("Thread Add")
+    
+    def checkExistenceIdentifier(self,identifier):
+        cont = 0
+        for i in range(len(self.identifier)):
+            if identifier[i] == identifier:
+                cont+=1
+            if cont>=2:
+                break
+                return True
+                
+        return False
         
